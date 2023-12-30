de82b8d5abc16ff6efea65313e28644702ec7671
MarouanSidali <marouane.sa.zenina@gmail.com>
2023-12-30 20:25:16 +0000

    Some modification on the ranked retrieval

--- SearchEngine/RankedRetrieval.py
+++ SearchEngine/RankedRetrieval.py
@@ -2,12 +2,21 @@ import math
 import re
 from index_loader import IndexLoader
 from collections import defaultdict
+from nltk.corpus import stopwords
+from nltk.stem import PorterStemmer
+import nltk
+
+nltk.download('stopwords')
 
 class TFIDFRankedRetrieval:
-    def __init__(self, index_file, queries_file, results_file):
+    def __init__(self, index_file, queries_file, results_file, do_stemming=True, do_stopwords_removal=True):
         self.index = IndexLoader.load_index(index_file)
         self.queries = self.load_queries(queries_file)
         self.results_file = results_file
+        self.do_stemming = do_stemming
+        self.do_stopwords_removal = do_stopwords_removal
+        self.stemmer = PorterStemmer()
+        self.stop_words = set(stopwords.words('english'))
 
     def load_queries(self, queries_file):
         queries = {}
@@ -22,12 +31,24 @@ class TFIDFRankedRetrieval:
                     queries[query_number] = query_text
         return queries
 
+    def preprocess_query(self, query):
+        query = query.lower()
+
+        if self.do_stemming:
+            query = " ".join(self.stemmer.stem(word) for word in query.split())
+
+        if self.do_stopwords_removal:
+            query = " ".join(word for word in query.split() if word not in self.stop_words)
+
+        return query
+
     def calculate_tfidf(self, term, doc_id):
         tf = len(self.index[term]['positions'][doc_id])
         idf = math.log10(len(self.index) / self.index[term]['df'])
         return (1 + math.log10(tf)) * idf
 
     def process_query(self, query_number, query_text):
+        query_text = self.preprocess_query(query_text)
         query_terms = re.findall(r'\b\w+\b', query_text.lower())
         scores = defaultdict(float)
 
@@ -37,7 +58,7 @@ class TFIDFRankedRetrieval:
                     scores[doc_id] += self.calculate_tfidf(term, doc_id)
 
         # Sort scores in descending order
-        sorted_results = sorted(scores.items(), key=lambda x: x[1], reverse=True)
+        sorted_results = sorted(scores.items(), key=lambda x: (x[1], x[0]), reverse=True)
 
         # Write the top 150 results to the results file
         with open(self.results_file, 'a') as file:
@@ -53,5 +74,5 @@ index_file = './SearchEngine/positional_inverted_index_v3.txt'
 queries_file = './SearchEngine/queries.ranked.txt'
 results_file = './SearchEngine/results.ranked.txt'
 
-ranked_retrieval = TFIDFRankedRetrieval(index_file, queries_file, results_file)
+ranked_retrieval = TFIDFRankedRetrieval(index_file, queries_file, results_file, do_stemming=True, do_stopwords_removal=True)
 ranked_retrieval.run_ranked_retrieval()
